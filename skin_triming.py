5010961fe630755283a9e24dfe579162de8a02a1
yamajunn <amiton1115@gmail.com>
2024-03-01 18:39:01 +0900

    aaa

--- skin_triming.py
+++ skin_triming.py
@@ -1,19 +1,19 @@
 import os
 import cv2
 
-dir_path = "SkinData/3/0"
+dir_path = "SkinData/data/girls_white/"
 
 files = os.listdir(dir_path)
 # print(files)
 
 for num, file in enumerate(files):
     img = cv2.imread(f'{dir_path}{file}')
-    # h, w = img.shape[:2]
-    # for i in range(h):
-    #     for j in range(w):
-    #         b, g, r = img[i, j]
-    #         if int(b)+int(g)+int(r) == 0:
-    #             img[i, j] = 255,255,255
+    h, w = img.shape[:2]
+    for i in range(h):
+        for j in range(w):
+            b, g, r = img[i, j]
+            if int(b)+int(g)+int(r) == 765:
+                img[i, j] = 255,0,0
     # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(f'SkinData/3/1/image{num}.png', img)
+    cv2.imwrite(f'SkinData/data/girls_blue/image{num}.png', img)
     # os.rename(f'SkinData/0/{file}', f"SkinData/1/image48.png")
