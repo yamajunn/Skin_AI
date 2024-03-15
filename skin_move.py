21cc38f72bb8b64a000710b3ec879a10452271dd
yamajunn <amiton1115@gmail.com>
2024-03-15 14:43:54 +0900

    aadddd

--- skin_move.py
+++ skin_move.py
@@ -2,8 +2,8 @@ import os
 import cv2
 
 file_path_1 = "SkinData/DataBox/alex_alpha/"
-# file_path_2 = "SkinData/DataBox/Axolotls/"
-move_path = "SkinData/data/alex/"
+file_path_2 = "SkinData/DataBox/Axolotls/"
+move_path = "SkinData/data/alex_axolotls/"
 
 files_1 = os.listdir(file_path_1)
 
@@ -11,9 +11,9 @@ for num, file in enumerate(files_1):
     img = cv2.imread(f'{file_path_1}{file}', cv2.IMREAD_UNCHANGED)
     cv2.imwrite(f'{move_path}image{num}.png', img)
 
-# files_len_1 = len(files_1)
+files_len_1 = len(files_1)
 
-# files_2 = os.listdir(file_path_2)
-# for num, file in enumerate(files_2):
-#     img = cv2.imread(f'{file_path_2}{file}', cv2.IMREAD_UNCHANGED)
-#     cv2.imwrite(f'{move_path}image{num+files_len_1}.png', img)
+files_2 = os.listdir(file_path_2)
+for num, file in enumerate(files_2):
+    img = cv2.imread(f'{file_path_2}{file}', cv2.IMREAD_UNCHANGED)
+    cv2.imwrite(f'{move_path}image{num+files_len_1}.png', img)
